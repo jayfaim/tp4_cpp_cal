7c619e20ed3329f9edff991bb21258bf68efe142
Unknown <pigeon.maxime1989@gmail.com>
2018-04-30 13:06:00 -0400

    modification .sh

--- tp4.sh
+++ tp4.sh
@@ -1,3 +1,12 @@
 #! /bin/bash
-g++ -o tp4 ./tp4.cpp ./tp4.hpp -std=c++11
-./tp4
+#g++ -o tp4 ./tp4.cpp ./tp4.hpp -std=c++11
+#./tp4
+
+#!/bin/bash
+# Commande pour compiler un programme c++
+#g++ --std=c++11 -c main.cpp fonctions.cpp #  <---------------- UBUNTU
+g++ -c testMain.cpp #<--------------------- Mint / Cygwin
+sleep 1
+g++ -o start testMain.o
+./start.exe
+exit 0
