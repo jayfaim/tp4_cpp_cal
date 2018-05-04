a0cab3c85cf24c92131dab51a80b83ded566bb18
jayfaim <jay.vrm.faim@gmail.com>
2018-05-03 21:13:44 -0400

    Passe les tests. J'ai fait du nettoyage dans le code. On pourrait rajouter plus de commentaires, y'en a pas beaucoup mais franchement jsuis correct avec le remettre comme il est

--- tp4.cpp
+++ tp4.cpp
@@ -2,13 +2,19 @@
 #include <iostream>
 #include <assert.h>
 using namespace std;
+/**
+********************************************************************************************
+Constructeurs et destructeur
+**/
 
+//Constructeur vide
 template <typename T>
 SmartPointer<T>::SmartPointer()
 {
 	*this->mNbRef = *this->mNbRef + 1;
 }
 
+//Constructeur avec valeur
 template <typename T>
 SmartPointer<T>::SmartPointer(T *ptr)
 {
@@ -16,15 +22,16 @@ SmartPointer<T>::SmartPointer(T *ptr)
 	this->mPtr = ptr;
 }
 
+//Constructeur avec valeur et fonction de destruction
 template <typename T>
-SmartPointer<T>::SmartPointer(T *ptr, DeleteFunctionType *dft)
+SmartPointer<T>::SmartPointer(T *ptr, DeleteFunctionType dft)
 {
 	*this->mNbRef = *this->mNbRef + 1;
 	this->mPtr = ptr;
 	this->mDeleteFunction = dft;
 }
 
-// Constructeur par copie.
+//Constructeur par copie
 template <typename T>
 SmartPointer<T>::SmartPointer(SmartPointer<T> &smp)
 {
@@ -33,41 +40,7 @@ SmartPointer<T>::SmartPointer(SmartPointer<T> &smp)
 	mPtr = smp.mPtr;
 }
 
-template <typename T>
-int SmartPointer<T>::getCount()
-{
-	cout << "test valeur *mPtr = " << *mPtr << endl;
-	return *this->mNbRef;
-}
-
-template <typename T>
-SmartPointer<T> &SmartPointer<T>::operator=(const SmartPointer<T> &copie)
-{
-	mNbRef = copie.mNbRef;
-	*mNbRef = *mNbRef + 1;
-	mPtr = copie.mPtr;
-	mDeleteFunction = copie.mDeleteFunction;
-	return *this;
-}
-
-template <typename T>
-bool SmartPointer<T>::operator==(const T &val)
-{
-	return *this->mPtr == val;
-}
-
-template <typename T>
-T *SmartPointer<T>::operator->()
-{
-	return this->mPtr;
-}
-
-template <typename T>
-T &SmartPointer<T>::operator*()
-{
-	return *this->mPtr;
-}
-
+//Destructeur
 template <typename T>
 SmartPointer<T>::~SmartPointer()
 {
@@ -80,103 +53,64 @@ SmartPointer<T>::~SmartPointer()
 		mDeleteFunction;
 	}
 }
+/**
+********************************************************************************************
+Opérateurs
+**/
 
+//Opérateur =
+template <typename T>
+SmartPointer<T> &SmartPointer<T>::operator=(const SmartPointer<T> &copie)
+{
+	mNbRef = copie.mNbRef;
+	*mNbRef = *mNbRef + 1;
+	mPtr = copie.mPtr;
+	mDeleteFunction = copie.mDeleteFunction;
+	return *this;
+}
+
+//Opérateur ==
+template <typename T>
+bool SmartPointer<T>::operator==(const T &val)
+{
+	return *this->mPtr == val;
+}
+
+//Opérateur ->
+template <typename T>
+T *SmartPointer<T>::operator->()
+{
+	return this->mPtr;
+}
+
+//Opérateur *
+template <typename T>
+T &SmartPointer<T>::operator*()
+{
+	return *this->mPtr;
+}
+
+//Opérateur[]
 template <typename T>
 T& SmartPointer<T>::operator[](int idx) {
 	return this->mPtr[idx];
+}
+
+/**
+********************************************************************************************
+Fonctions d'accès aux données du pointeur et fonctions de destruction
+**/
+
+template <typename T>
+int SmartPointer<T>::getCount()
+{
+	return *this->mNbRef;
 }
 
 template <typename T>
 int SmartPointer<T>::size(){
 	size_t size = (sizeof this->mPtr / sizeof this->mPtr[0]);
 	return size;
-}
-
-int main()
-{
-	cout << "Fuck you!!" << endl;
-	cout << "***********tests jay*************" << endl;
-	SmartPointer<int> test1(new int(4));
-	SmartPointer<int> test2;
-	test2 = test1;
-	int tmp = test2.getCount();
-	cout << "nbref = " << tmp << endl;
-	tmp = test1.getCount();
-	cout << "nbref = " << tmp << endl;
-	SmartPointer<string> test3(new string("je suis une string test"));
-	cout << "taille de test3 = " << test3->size() << endl;
-	//string *str_tmp = *test3;
-	//cout << "test operateur * test3 = " << *str_tmp << endl;
-	cout << "" << endl;
-	cout << "" << endl;
-	cout << "**********tests max**************" << endl;
-	int foo[5] = {16, 2, 77, 40, 12071};
-	SmartPointer<int> test5(new int(4));
-	//int *patate = *test5;
-
-	//SmartPointer < int > tab ( new int [7] , SmartPointer < int >::deleteTab ) ;
-
-	//cout << "Fuck you!!" << *patate << endl;
-
-	// TEST LE COMPTE DE REFERENCES
-	cout << "Début test références...";
-	SmartPointer<int> sm(new int(4));
-	SmartPointer<int> sm2 = sm;
-	assert(sm2.getCount() == 2 && sm.getCount() == 2);
-	SmartPointer<int> sm3;
-	SmartPointer<int> sm4;
-	cout << "Ok!" << endl;
-	// TEST surcharge de =
-	cout << "Début test surcharge de =...";
-	sm3 = sm2;
-	assert(sm3.getCount() == 3 && sm2.getCount() == 3);
-	assert(sm.getCount() == 3);
-	sm4 = sm2;
-	assert(sm4.getCount() == 4 && sm3.getCount() == 4);
-	assert(sm2.getCount() == 4 && sm.getCount() == 4);
-	cout << "Ok!" << endl;
-	// TEST LE DESTRUCTEUR
-	cout << "Début test destructeur...";
-	SmartPointer<std ::string> smartPS3;
-	{
-		SmartPointer<std ::string> smartPS(new string("bonjour"));
-		SmartPointer<std ::string> smartPS2 = smartPS;
-		smartPS3 = smartPS2;
-	}
-	assert(smartPS3.getCount() == 1);
-	cout << "Ok!" << endl;
-
-	// TEST surcharge de *
-	cout << "Début du test de surcharge de *...";
-	cout << "AVANT SON SMARTPS3" << *smartPS3 << endl;
-	assert(*smartPS3 == "bonjour");
-	assert(*sm4 == 4);
-	*sm4 = 3;
-	assert(*sm3 == 3);
-	cout << "Ok!" << endl;
-
-	
-	// TEST surcharge de ->
-	cout << "Début du test de surcharge de ->...";
-	int length = smartPS3->size();
-	cout << "BLABLA" << length << endl;
-	assert ( length == 7) ;
-	cout << "Ok!" << endl;
-	// TEST surcharge de []
-	
-	
-	cout << "Début de test de surcharge de []...";
-	//int *tab = new int[7];
-	//int size = (sizeof *tab / sizeof tab[0]); // size == 5
-	//cout << "BLABLABLALBALB " << tab->size() << " AFKOSDKSAODKADOASK " << sizeof tab[0] << endl;
-	//SmartPointer<int> tabTest (new int[7]);
-	//cout << "Patate" << tabTest.size() << endl;
-	//SmartPointer<int> tab(new int[7], SmartPointer<int>::deleteTab);
-	//tab[0] = 0;
-	//cout << tab[0] << endl ;
-	//assert (tab.getCount()==1) ;
-	//cout << "Ok!" << endl;
-	return 0;
 }
 
 template <typename T>
@@ -190,3 +124,66 @@ void SmartPointer<T>::deleteTab(T *ptr)
 {
 	delete[] ptr;
 }
+
+/**
+********************************************************************************************
+Boucle principale
+**/
+
+int main()
+{
+	// TEST LE COMPTE DE REFERENCES
+	cout << "Début test références...";
+	SmartPointer<int> sm(new int(4));
+	SmartPointer<int> sm2 = sm;
+	assert(sm2.getCount() == 2 && sm.getCount() == 2);
+	SmartPointer<int> sm3;
+	SmartPointer<int> sm4;
+	cout << "Ok!" << endl;
+	
+	// TEST surcharge de =
+	cout << "Début test surcharge de =...";
+	sm3 = sm2;
+	assert(sm3.getCount() == 3 && sm2.getCount() == 3);
+	assert(sm.getCount() == 3);
+	sm4 = sm2;
+	assert(sm4.getCount() == 4 && sm3.getCount() == 4);
+	assert(sm2.getCount() == 4 && sm.getCount() == 4);
+	cout << "Ok!" << endl;
+	
+	// TEST LE DESTRUCTEUR
+	cout << "Début test destructeur...";
+	SmartPointer<std ::string> smartPS3;
+	{
+		SmartPointer<std ::string> smartPS(new string("bonjour"));
+		SmartPointer<std ::string> smartPS2 = smartPS;
+		smartPS3 = smartPS2;
+	}
+	assert(smartPS3.getCount() == 1);
+	cout << "Ok!" << endl;
+
+	// TEST surcharge de *
+	cout << "Début du test de surcharge de *...";
+	assert(*smartPS3 == "bonjour");
+	assert(*sm4 == 4);
+	*sm4 = 3;
+	assert(*sm3 == 3);
+	cout << "Ok!" << endl;
+
+	
+	// TEST surcharge de ->
+	cout << "Début du test de surcharge de ->...";
+	int length = smartPS3->size();
+	assert ( length == 7) ;
+	cout << "Ok!" << endl;
+	
+	// TEST surcharge de []
+	cout << "Début de test de surcharge de []...";
+	SmartPointer<int> tabTest (new int[7]);
+	SmartPointer<int> tab(new int[7], SmartPointer<int>::deleteTab);
+	tab[0] = 0;
+	assert (tab.getCount()==1) ;
+	cout << "Ok!" << endl;
+	return 0;
+}
+
--- tp4.hpp
+++ tp4.hpp
@@ -12,49 +12,46 @@
   {
     private :
       // un pointeur vers l’emplacement mémoire qui est “gardé” par l’instance SmartPointer.
-      // Je suis pas sur du type des variables.
       T *mPtr;
       
       // un pointeur sur un entier compteur du nombre de références à mPtr.
       int *mNbRef = new int(0);
 
-      //J'ai bougé la redéfinition avant qu'on l'appelle sinon ca plante
-      //Voici une définition qui pourra être utile pour la déclaration de vos pointeurs sur les fonctions statiques :
-      //Cela définit le type DeleteFunctionType comme un pointeur sur une fonction void prenant
+      //Définit le type DeleteFunctionType comme un pointeur sur une fonction void prenant
       //en argument un pointeur de T, exactement le type des fonctions deletePtr et deleteTab.
-      
       typedef void (* DeleteFunctionType )(T *p);
       
       
       // un pointeur vers une fonction statique de destruction du pointeur.
-      DeleteFunctionType* mDeleteFunction;
+      DeleteFunctionType mDeleteFunction;
     
       public :
-      //Constructeur vide
+      //Constructeur vide.
       SmartPointer<T>();
 
+      //Constructeur avec valeur.
       SmartPointer<T>(T*);
 
-      //Constructeur avec pointeur vers fonction de destruction et la donnée
-      SmartPointer<T>(T*, DeleteFunctionType*);
+      //Constructeur avec pointeur vers fonction de destruction et la donnée.
+      SmartPointer<T>(T*, DeleteFunctionType);
       
       //Constructeur par copie(prend une référence vers un autre smartpointer)
       SmartPointer<T>(SmartPointer<T>&);
       
+      //Fonctions de destruction passées au constructeur
       static void deletePtr(T*);
       static void deleteTab(T*);
       
+      //Opérateurs
       ~SmartPointer<T>();
       SmartPointer<T>& operator=(const SmartPointer<T>&);
       bool operator==(const T&);
 	    T* operator->();
 	    T& operator*();
       T& operator[](int idx);
- 
-      // — La fonction getCount(), qui vous donnera accès au nombre de références à mPtr.
-      //  Cette fonction pourra être utilisée durant le débogage avec “assert”. 
+
+      //Fonctions d'accès
       int getCount();
-      
       int size();
   };
 
